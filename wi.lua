1214f34b602c67bff822549e2094edb606643ec9
captainju <blondeau.ju@gmail.com>
2014-03-26 15:53:08 +0100

    update widgets

--- wi.lua
+++ wi.lua
@@ -47,17 +47,17 @@ memwidget = awful.widget.progressbar()
 memwidget:set_width(8)
 memwidget:set_height(10)
 memwidget:set_vertical(true)
-memwidget:set_background_color("#494B4F")
+memwidget:set_background_color("#222222")
 memwidget:set_border_color(nil)
 memwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#AECF96"}, {0.5, "#88A175"},  {1, "#FF5656"}}})
-vicious.register(memwidget, vicious.widgets.mem, "$1", 13)
+vicious.register(memwidget, vicious.widgets.mem, "$1", 2)
 
 -- CPU usage graph
 cpuwidget = awful.widget.graph()
 cpuwidget:set_width(50)
-cpuwidget:set_background_color("#000000")
+cpuwidget:set_background_color("#222222")
 cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, {1, "#AECF96" }}})
-vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
+vicious.register(cpuwidget, vicious.widgets.cpu, "$1", 0.5)
 
 
 -- {{{ BATTERY
